166c73ae4d3c54040316ff2b6e93e83c21c2632d
Heloisa Duarte <heloisaa.sdp@gmail.com>
2025-08-19 18:59:29 -0300

    JS - Update

--- src/script/script.js
+++ src/script/script.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItens = $('.nav_item');
+
+    $(window).on('scroll', function() {
+        const header =$('header');
+
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        console.log(scrollPosition);
+        });
 });
